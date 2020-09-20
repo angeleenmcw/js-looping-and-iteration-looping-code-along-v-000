4efd4ec8ca660eaa8dabb37b4205e678f19b6427
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-20 06:25:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const writeCards = ["Ada","Brendan","Ali"];
+
+function writecard(writeCards) {
+  console.log("Thank you, ${writecards}, for the wonderful birthday gift!");
+  return writeCards
+}
+
+writecard(writeCards);
+
+let countdown = 10;
+
+while (countdown > 0) {
+  console.log(countdown++);
+  
+}
